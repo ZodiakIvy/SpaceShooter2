8f2608cf65f0330f90c20fcaa28a1d46683a9afe
Xavier Hadley <zodiakivy@hotmail.com>
2023-08-27 09:33:29 -0400

    Added Asteroid and explosion logic

--- Assets/Scripts/AsteroidBehaviour.cs
+++ Assets/Scripts/AsteroidBehaviour.cs
@@ -7,12 +7,26 @@ public class AsteroidBehaviour : MonoBehaviour
     [SerializeField]
     private GameObject _asteroid;
     [SerializeField]
-    private float _speed = 3f;
+    private float _rotateSpeed = 19f;
+    [SerializeField]
+    private float _moveSpeed = .5f;
+    private Animator _anim;
+    private  PlayerBehaviour _player;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _player = GameObject.Find("Player").GetComponent<PlayerBehaviour>();
+        if (_player == null)
+        {
+            Debug.LogError("Player is NULL");
+        }
+        _anim = GetComponent<Animator>();
+        if (_anim == null)
+        {
+            Debug.LogError("The Animator is NULL");
+        }
     }
 
     // Update is called once per frame
@@ -23,13 +37,33 @@ public class AsteroidBehaviour : MonoBehaviour
 
     void AsteroidMovement()
     {
-        transform.Rotate(new Vector3(0, 0, 3) * _speed * Time.deltaTime); 
-        if (transform.position.y <= -5.3f)
+        transform.Rotate(new Vector3(0, 0, 1) * _rotateSpeed * Time.deltaTime);
+        transform.position += (new Vector3(1, 0, 0) * _moveSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Hit: " + other.transform.name);
+        if (other.tag == "Laser")
         {
-            float randomX = Random.Range(-8.5f, 7.6f);
-            float randomY = Random.Range(-3.3f, 7f);
-            Vector3 asteroidPosition = transform.position + new Vector3(randomX, randomY, 0);
-            GameObject newAsteroid = Instantiate(_asteroid, asteroidPosition, Quaternion.Euler(0f, 0f, 3f));
+            Destroy(other.gameObject);
+
+            _anim.SetTrigger("OnEnemyDeath");
+            _moveSpeed = 0;
+
+            Destroy(this.gameObject, 2.4f);
+
         }
+
+        if (other.tag == "Player")
+        {
+            PlayerBehaviour player = other.transform.GetComponent<PlayerBehaviour>();
+            other.transform.GetComponent<PlayerBehaviour>().Damage();
+            _anim.SetTrigger("OnEnemyDeath");
+            _moveSpeed = 0;
+            Destroy(this.gameObject, 2.4f);
+        }
+
+
     }
 }
